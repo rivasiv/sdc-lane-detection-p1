47b1184fca209631f9c7df891c0de928047b3ca6
Sameh Mohamed <sameh@autojazari.com>
2016-10-28 01:38:00 -0400

    TODO: refactor the slope and line extension code

--- main.py
+++ main.py
@@ -15,12 +15,13 @@ import cv2
 
 def load_image(image_path):
     """ pass for now """
-    return  mpimg.imread(image_path)
+    #return (mpimg.imread(image_path)*255).astype('uint8')
+    return mpimg.imread(image_path)
 
 
-def save_plot(name, img):
+def save_plot(dir_name, name, img):
     """ save plot to current directory """
-    cv2.imwrite(name, img)
+    cv2.imwrite(join(dir_name, name), img)
 
 
 def grayscale(img):
@@ -30,15 +31,14 @@ def grayscale(img):
     you should call plt.imshow(gray, cmap='gray')"""
     return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+def gaussian_noise(img, kernel_size):
+    """ Applies a Gaussian Noise kernel """
+    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+
 
 def canny(img, low_threshold, high_threshold):
     """ Applies the Canny transform """
     return cv2.Canny(img, low_threshold, high_threshold)
-
-
-def gaussian_noise(img, kernel_size):
-    """ Applies a Gaussian Noise kernel """
-    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
 
 
 def region_of_interest(img, vertices):
@@ -66,7 +66,7 @@ def region_of_interest(img, vertices):
     return masked_image
 
 
-def draw_lines(img, lines, color=(255, 0, 0), thickness=2):
+def draw_lines(img, lines, color=(255, 0, 0), thickness=1):
     """
     NOTE: this is the function you might want to use as a starting point once you want to
     average/extrapolate the line segments you detect to map out the full
@@ -83,9 +83,46 @@ def draw_lines(img, lines, color=(255, 0, 0), thickness=2):
     If you want to make the lines semi-transparent, think about combining
     this function with the weighted_img() function below
     """
+    right_slope = []
+    left_slope = []
+
+    left_lines = []
+    right_lines = []
+
     for line in lines:
         for x1, y1, x2, y2 in line:
-            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+            m = ((y2-y1)/(x2-x1)) # slope
+            if m < 0:
+                left_slope.append(m)
+                left_lines.append((x2,y2))
+            else:
+                right_slope.append(m)
+                right_lines.append((x1,y1))
+    
+    # average left and right slopes
+    right_slope = sorted(right_slope)[int(len(right_slope)/2)]
+    left_slope = sorted(left_slope)[int(len(left_slope)/2)]
+
+    left_y1 = min([line[1] for line in left_lines])
+    left_pair = tuple([line[0] for line in left_lines if line[1] == left_y1] + [left_y1])
+
+    right_y1 = min([line[1] for line in right_lines])
+    right_pair = tuple([line[0] for line in right_lines if line[1] == right_y1] + [right_y1])
+
+    # x2 = ((y2-y1)/m) + x1 where y2 = max height
+    left_x = int((img.shape[1]-left_pair[1])/left_slope) + left_pair[0]
+    right_x = int((img.shape[1]-right_pair[1])/right_slope) + right_pair[0]
+    
+    cv2.line(img, left_pair, (left_x, img.shape[1]), color, thickness)
+    cv2.line(img, right_pair, (right_x, img.shape[1]), color, thickness)
+        
+    #left_y = int(-(m*left_min_x) - left_min_y)
+    
+    #right_y = int((right_slope * (right_max_x)) + right_max_y)
+
+    #    cv2.line(img, (0,left_y), (left_min_x, left_min_y), color, thickness)
+    #cv2.line(img, (0,right_y), (right_max_x, right_max_y), color, thickness)
+    #cv2.line(img, (x1, y1), (x2, y2), color, thickness)
 
 
 def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
@@ -101,7 +138,7 @@ def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
     return line_img
 
 
-def weighted_img(img, initial_img, alpha=0.8, beta=1., upsilon=0.):
+def weighted_img(edges, line_img, alpha=0.8, beta=1., upsilon=0.):
     """
     Python 3 has support for cool math symbols.
     `img` is the output of the hough_lines(), An image with lines drawn on it.
@@ -114,7 +151,8 @@ def weighted_img(img, initial_img, alpha=0.8, beta=1., upsilon=0.):
     initial_img * alpha + img * beta + upsilon 
     NOTE: initial_img and img must be the same shape!
     """
-    return cv2.addWeighted(initial_img, alpha, img, beta, upsilon)
+    color_edges = np.dstack((edges, edges, edges))
+    return cv2.addWeighted(color_edges, alpha, line_img, beta, upsilon)
 
 
 def parse_args():
@@ -140,26 +178,43 @@ def process_img(name, img):
     # 3: Hough transform
     # 4: Apply ROI
     """
+    dir_name = "output_images"
+    line_image = np.copy(img)*0
+ 
     gray_img = grayscale(img)
-    save_plot("gray_"+name, gray_img)
+    #save_plot(dir_name, "gray_"+name, gray_img)
 
-    blur_img = gaussian_noise(gray_img, 5)
-    save_plot("blur_"+name, blur_img)
+    blur_gray = gaussian_noise(gray_img, 3)
+    #save_plot(dir_name, "blur_"+name, blur_gray)
 
-    edges = canny(blur_img, 50, 150)
-    save_plot("edges_"+name, edges)
+    edges = canny(blur_gray, 50, 150)
+    #save_plot(dir_name, "edges_"+name, edges)
 
     imshape = img.shape
-    #vertices = np.array([[(160,imshape[0]),(490,290),(450,290), (960,540)]], dtype=np.int32)
-    vertices = np.array([[(160,imshape[0]),(490,290),(450,290), (imshape[1],imshape[0])]], dtype=np.int32)
+    print(imshape[0], imshape[1])
+    vertices = np.array([[(110,imshape[0]),(410, 310),(480, 310), (imshape[1],imshape[0])]], dtype=np.int32)
     masked_edges = region_of_interest(edges, vertices)
-    save_plot("roi_"+name, masked_edges)
     
-    lines = hough_lines(masked_edges, 1, np.pi/180, 15, 120, 90)
-    save_plot("hough_"+name, lines)
+    #save_plot(dir_name, "roi_"+name, masked_edges)
+    
+    lines = hough_lines(masked_edges, .1, np.pi/180, 15, 120, 80)
+    #save_plot(dir_name, "hough_"+name, lines)
 
-    final_img = weighted_img(lines, img)
-    save_plot("final_"+name, final_img)
+    final_img = weighted_img(lines, line_image)
+    #save_plot(dir_name, "final_"+name, final_img)
+
+    #images = (gray_img, blur_gray, edges, masked_edges, lines, final_img)
+    #i = 0
+    images = (final_img,)
+    i = 1
+    for _image in images:
+        plt.ion()
+        if i == 0:
+            i += 1
+            plt.imshow(_image, cmap='gray')
+        else:
+            plt.imshow(_image)
+        plt.show(block=True)
 
 
 def main():
